a06e67b454e1157dd7105b73d06c50d3a990ece5
Neonsy <neonsy@users.noreply.github.com>
2025-01-10 00:00:34 +0100

    Setup Prettier

--- project/eslint.config.js
+++ project/eslint.config.js
@@ -1,15 +1,18 @@
-import eslintPluginAstro from 'eslint-plugin-astro';
-import eslintPluginJsxA11y from 'eslint-plugin-jsx-a11y';
+import eslintPluginAstro from "eslint-plugin-astro";
+import eslintPluginJsxA11y from "eslint-plugin-jsx-a11y";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default [
-    // add more generic rule sets here, such as:
-    // js.configs.recommended,
-    ...eslintPluginAstro.configs.recommended,
-    ...eslintPluginJsxA11y.configs.recommended,
-    {
-        rules: {
-            // override/add rules settings here, such as:
-            // "astro/no-set-html-directive": "error"
-        },
+  // add more generic rule sets here, such as:
+  // js.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
+  ...eslintPluginJsxA11y.configs.recommended,
+  {
+    rules: {
+      // override/add rules settings here, such as:
+      // "astro/no-set-html-directive": "error"
     },
+  },
+  // This disables rules that conflict with Prettier
+  eslintConfigPrettier,
 ];
