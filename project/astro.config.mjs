a2d6f4d2cd7e92215302dd09e927ddf8082b5473
Neonsy <118444485+Neonsy@users.noreply.github.com>
2025-01-11 20:55:27 +0100

    Added Vercel Adapter

--- project/astro.config.mjs
+++ project/astro.config.mjs
@@ -2,11 +2,12 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
-
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), react()],
-    site: 'https://astro-neonspace.vercel.app',
+  integrations: [tailwind(), react()],
+  site: 'https://astro-neonspace.vercel.app',
+  adapter: vercel(),
 });
