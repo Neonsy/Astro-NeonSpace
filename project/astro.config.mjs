be477030956413774ba170f6d27e5055a8ed0030
Neonsy <118444485+Neonsy@users.noreply.github.com>
2025-01-11 06:55:23 +0000

    Updated Markdown Information Added Site in Astro Config

--- project/astro.config.mjs
+++ project/astro.config.mjs
@@ -8,4 +8,5 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
     integrations: [tailwind(), react()],
+    site: 'https://astro-neonspace.vercel.app',
 });
