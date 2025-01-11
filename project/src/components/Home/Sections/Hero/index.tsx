5e51aa3a99f12f3d1923849f8625459779bf0ca7
Neonsy <118444485+Neonsy@users.noreply.github.com>
2025-01-11 22:42:10 +0100

    Preparing scroll effect on Hero section

--- project/src/components/Home/Sections/Hero/index.tsx
+++ project/src/components/Home/Sections/Hero/index.tsx
@@ -1,6 +1,17 @@
+import { motion, useScroll, useTransform } from 'motion/react';
+
 export default function Hero() {
+    const { scrollYProgress } = useScroll();
+
+    const opacity = useTransform(scrollYProgress, [0, 5], [1, 0]);
+    const y = useTransform(scrollYProgress, [0, 0.5], [0, -500]);
+
     return (
         <section>
+            <motion.div style={{ opacity, y }} className='pointer-events-none absolute inset-0'>
+                <div className='absolute left-0 top-0 h-full w-full bg-gradient-to-br from-[#1a1b4b] to-[#4b1b66] opacity-30' />
+            </motion.div>
+
             <div className='flex select-none flex-col items-center justify-center'>
                 <h1 className='from-logo-gradient-1 via-logo-gradient-2 to-logo-gradient-3 w-fit bg-gradient-to-r bg-clip-text text-9xl font-bold text-transparent'>
                     NeonSpace
