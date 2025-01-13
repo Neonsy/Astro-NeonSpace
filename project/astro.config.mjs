aada3e2b25227143955c4a18a124e768d8beddbe
Neonsy <118444485+Neonsy@users.noreply.github.com>
2025-01-13 17:51:07 +0100

    Added React compiler

--- project/astro.config.mjs
+++ project/astro.config.mjs
@@ -5,9 +5,25 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import vercel from '@astrojs/vercel';
 
+const ReactCompilerConfig = {
+    // Optional: configure specific directories
+    // sources: (filename) => {
+    //   return filename.indexOf('src/components') !== -1;
+    // },
+    // Optional: specify React version if not using React 19
+    // target: '18'
+};
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
-  site: 'https://astro-neonspace.vercel.app',
-  adapter: vercel(),
+    integrations: [
+        tailwind(),
+        react({
+            babel: {
+                plugins: [['babel-plugin-react-compiler', ReactCompilerConfig]],
+            },
+        }),
+    ],
+    site: 'https://astro-neonspace.vercel.app',
+    adapter: vercel(),
 });
