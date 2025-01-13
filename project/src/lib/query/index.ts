66909fc9d5b8aad3aa7405968ff4c59b38c3c597
Neonsy <118444485+Neonsy@users.noreply.github.com>
2025-01-13 19:40:03 +0100

    Configured QueryClient & Created draft Types for GithubStats

--- project/src/lib/query/index.ts
+++ project/src/lib/query/index.ts
@@ -1,3 +1,13 @@
 import { QueryClient } from '@tanstack/react-query';
 
-export const queryClient = new QueryClient();
+export const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            refetchOnWindowFocus: true,
+            refetchOnMount: true,
+            refetchOnReconnect: true,
+            staleTime: 1000 * 60 * 60 * 24, // 24 hours
+            gcTime: 1000 * 60 * 5, // 5 minutes
+        },
+    },
+});
