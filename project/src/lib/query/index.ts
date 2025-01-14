489c34a110ef921302775043717709d242aeec8a
Neonsy <118444485+Neonsy@users.noreply.github.com>
2025-01-14 18:17:37 +0100

    Changed default Query options

--- project/src/lib/query/index.ts
+++ project/src/lib/query/index.ts
@@ -6,7 +6,7 @@ export const queryClient = new QueryClient({
             refetchOnWindowFocus: true,
             refetchOnMount: true,
             refetchOnReconnect: true,
-            staleTime: 1000 * 60 * 60 * 24, // 24 hours
+            staleTime: 1000 * 60 * 60 * 1, // 1 hour
             gcTime: 1000 * 60 * 5, // 5 minutes
         },
     },
