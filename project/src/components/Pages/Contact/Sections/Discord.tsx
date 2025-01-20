4c57700d62fc2a1975541a44bd38ea7f9dee581f
Neonsy <118444485+Neonsy@users.noreply.github.com>
2025-01-20 19:47:09 +0100

    Contact page done

--- project/src/components/Pages/Contact/Sections/Discord.tsx
+++ project/src/components/Pages/Contact/Sections/Discord.tsx
@@ -1,4 +1,21 @@
-import { FaCode, FaRocket, FaHashtag, FaPeopleGroup } from 'react-icons/fa6';
+import {
+    FaCode,
+    FaRocket,
+    FaHashtag,
+    FaPeopleGroup,
+    FaHouse,
+    FaGlobe,
+    FaCubes,
+    FaChartBar,
+    FaServer,
+    FaGamepad,
+    FaPaintbrush,
+    FaMicrochip,
+    FaLifeRing,
+    FaTicketSimple,
+} from 'react-icons/fa6';
+import { FaTools } from 'react-icons/fa';
+
 import { MdChatBubble } from 'react-icons/md';
 
 import Link from '@/components/Common/Link';
@@ -9,63 +26,107 @@ export default function Discord() {
     return (
         <section className='flex flex-col items-center justify-center gap-y-6 py-24'>
             <div className='flex flex-col items-center justify-center gap-y-6'>
-                <h1 className='gradient-text font-bold ~text-5xl/9xl'>NeonTechSpace</h1>
+                <h1 className='gradient-text font-bold ~text-4xl/9xl'>NeonTechSpace</h1>
             </div>
-            <div className='bg-discord-primary/15 flex flex-col justify-center gap-y-12 p-12 lg:backdrop-blur-sm'>
-                <div className='flex flex-col items-center gap-y-2.5'>
-                    <h2 className='flex items-center justify-center gap-x-2 ~text-2xl/5xl'>
-                        <IconStyle icon={MdChatBubble} />
-                        Discord Community
-                    </h2>
-                    <p className='max-w-md text-text-secondary ~text-base/xl'>
-                        Join the community and connect with fellow developers, creators and enthusiasts, in this vibrant discord server.
-                    </p>
+            <div className='flex flex-col justify-center gap-y-12 p-12 lg:flex-row'>
+                <div className='flex flex-col items-center gap-y-12'>
+                    <div className='flex flex-col items-center gap-y-2.5 px-3'>
+                        <h2 className='flex items-center justify-center gap-x-2 ~text-xl/5xl'>
+                            <IconStyle icon={MdChatBubble} />
+                            Discord Community
+                        </h2>
+                        <p className='max-w-md text-center text-text-secondary ~text-sm/xl'>
+                            Join the community and connect with fellow developers, creators and enthusiasts, in this vibrant discord server.
+                        </p>
+                    </div>
+                    <JoinServerButton className='hidden lg:flex' />
                 </div>
-                <div className='flex items-center justify-between'>
-                    <div className='flex flex-col gap-y-6'>
-                        <h2 className='~text-2xl/5xl'>Why Join Us</h2>
-                        <ul className='flex flex-col gap-y-3'>
-                            <li className='flex items-center gap-x-3 ~text-base/xl'>
-                                <FaRocket className='text-logo-gradient-1' />
-                                Connect with like-minded developers
-                            </li>
-                            <li className='flex items-center gap-x-3 ~text-base/xl'>
-                                <FaCode className='text-logo-gradient-1' />
-                                Create or Join collaborative projects
-                            </li>
-                            <li className='flex items-center gap-x-3 ~text-base/xl'>
-                                <FaPeopleGroup className='text-logo-gradient-1' />
-                                Start a discussion about various topics
-                            </li>
-                            <li className='flex items-center gap-x-3 ~text-base/xl'>
-                                <FaHashtag className='text-logo-gradient-1' />
-                                Teach, grow and learn from each other
-                            </li>
-                        </ul>
-                    </div>
-                    <div className='flex flex-col items-center gap-y-2.5'>
-                        <Link
-                            className='bg-discord-primary flex items-center justify-center gap-x-3 rounded-xl px-5 py-3 font-bold ~text-base/3xl'
-                            href='https://discord.gg/neonsy'
-                            external>
-                            <MdChatBubble /> Join Discord Server
-                        </Link>
-                        <p className='max-w-xs text-center ~text-sm/xl'>Already a member? Open Discord to chat!</p>
-                    </div>
+                <div className='flex flex-col items-center justify-center gap-y-9 px-3'>
+                    <h2 className='~text-xl/5xl'>Why Join Us</h2>
+                    <ul className='flex flex-col gap-y-3'>
+                        <li className='flex items-center gap-x-3 ~text-sm/xl'>
+                            <FaRocket className='text-logo-gradient-1' />
+                            Connect with like-minded developers
+                        </li>
+                        <li className='flex items-center gap-x-3 ~text-sm/xl'>
+                            <FaCode className='text-logo-gradient-1' />
+                            Create or Join collaborative projects
+                        </li>
+                        <li className='flex items-center gap-x-3 ~text-sm/xl'>
+                            <FaPeopleGroup className='text-logo-gradient-1' />
+                            Start a discussion about various topics
+                        </li>
+                        <li className='flex items-center gap-x-3 ~text-sm/xl'>
+                            <FaHashtag className='text-logo-gradient-1' />
+                            Teach, grow and learn from each other
+                        </li>
+                    </ul>
+                    <JoinServerButton className='lg:hidden' />
                 </div>
-                <div className='flex flex-col items-center justify-center gap-y-6'>
-                    <h2 className='~text-2xl/5xl'>Server Features</h2>
-                    <div className='grid grid-cols-3 gap-4'>
-                        <FeatureCard title='Home' icon={FaCode} description='News, issues, suggestions, and custom roles' />
-                        <FeatureCard title='General' icon={FaCode} description='Casual conversations and community interactions' />
-                        <FeatureCard title='Programming' icon={FaCode} description='Discuss various programming languages' />
-                        <FeatureCard title='Data Science' icon={FaCode} description='Data analysis and machine learning discussions' />
-                        <FeatureCard title='Server & Tools' icon={FaCode} description='Server management and DevOps tools' />
-                        <FeatureCard title='Game Dev' icon={FaCode} description='Game design and development discussions' />
-                        <FeatureCard title='Design' icon={FaCode} description='Creative projects and design feedback' />
-                        <FeatureCard title='Specialized Topics' icon={FaCode} description='Discuss specialized topics like AI, career advice, ask for code reviews, etc.' />
-                        <FeatureCard title='Resources &Tickets' icon={FaCode} description='Private and public support channels' />
-                    </div>
+            </div>
+            <div className='flex flex-col items-center justify-center gap-y-6'>
+                <h2 className='~text-2xl/5xl'>Server Features</h2>
+                <div className='grid grid-cols-1 gap-4 lg:grid-cols-3'>
+                    <FeatureCard
+                        title='Home'
+                        icon={FaHouse}
+                        description='Your first stop for server news, role selection, issue reporting, and community suggestions'
+                    />
+                    <FeatureCard
+                        title='General'
+                        icon={FaGlobe}
+                        description='The heart of our community where casual conversations and friendships bloom'
+                    />
+                    <FeatureCard
+                        title='Programming Languages'
+                        icon={FaCode}
+                        description='Deep dive into various programming languages, from Python to Rust and everything in between'
+                    />
+                    <FeatureCard
+                        title='Frameworks & Libraries'
+                        icon={FaCubes}
+                        description='Explore the vast ecosystem of development frameworks and libraries together'
+                    />
+                    <FeatureCard
+                        title='Development'
+                        icon={FaTools}
+                        description='Level up your development workflow with tools, tips, and best practices'
+                    />
+                    <FeatureCard
+                        title='Data Science'
+                        icon={FaChartBar}
+                        description='Unlock insights through data analysis, big data tech, and machine learning'
+                    />
+                    <FeatureCard
+                        title='Server & DevOps'
+                        icon={FaServer}
+                        description='Master server management, OS configurations, and DevOps practices'
+                    />
+                    <FeatureCard
+                        title='Game Development'
+                        icon={FaGamepad}
+                        description='Create amazing games with Unity, Unreal Engine, and other game dev tools'
+                    />
+                    <FeatureCard
+                        title='Design & Creativity'
+                        icon={FaPaintbrush}
+                        description='Showcase your creative projects and get feedback on designs, 3D models, and videos'
+                    />
+                    <FeatureCard
+                        title='Specialized Topics'
+                        icon={FaMicrochip}
+                        description='Dive into AI, career advice, code reviews, and other specialized discussions'
+                    />
+                    <FeatureCard
+                        title='Support & Resources'
+                        icon={FaLifeRing}
+                        description='Access helpful resources and get support from our knowledgeable community'
+                    />
+                    <FeatureCard
+                        title='Tickets'
+                        icon={FaTicketSimple}
+                        description='Get personalized help through private staff tickets or community support channels'
+                    />
                 </div>
             </div>
         </section>
@@ -93,7 +154,21 @@ function FeatureCard({ icon: Icon, title, description }: FeatureCardProps) {
                 <IconStyle icon={Icon} />
                 <h4 className='~text-base/3xl'>{title}</h4>
             </h3>
-            <p className='max-w-sm ~text-base/xl'>{description}</p>
+            <p className='max-w-xs text-center ~text-base/lg'>{description}</p>
+        </div>
+    );
+}
+
+function JoinServerButton({ className }: { className?: string }) {
+    return (
+        <div className={`flex flex-col items-center gap-y-3 px-3 ${className}`}>
+            <Link
+                className='bg-discord-primary flex items-center justify-center gap-x-3 gap-y-3 rounded-xl ~px-2/5 ~py-2/3 font-bold ~text-sm/3xl'
+                href='https://discord.gg/neonsy'
+                external>
+                <MdChatBubble /> Join Discord Server
+            </Link>
+            <p className='max-w-xs text-center ~text-xs/xl'>Already a member? Open Discord to chat!</p>
         </div>
     );
 }
