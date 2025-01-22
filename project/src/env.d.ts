6527afeac3bde8cbb3438795c5c97d30d5d276ea
Neonsy <118444485+Neonsy@users.noreply.github.com>
2025-01-22 16:44:47 +0100

    Added drizzle ORM and turso Added migration to deploy

--- project/src/env.d.ts
+++ project/src/env.d.ts
@@ -3,4 +3,7 @@ interface ImportMetaEnv {
 
     readonly PUBLIC_CLERK_PUBLISHABLE_KEY: string;
     readonly CLERK_SECRET_KEY: string;
+
+    readonly TURSO_DATABASE_URL: string;
+    readonly TURSO_AUTH_TOKEN: string;
 }
